6e88cfbe55547f286800c20dad48f42931bf40ea
hamzatuna <tuanaham@itu.edu.tr>
2018-06-24 15:46:47 +0300

    astar is added

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import time
 import os
-from generics.generic import  bfs, dfs
+from generics.generic import  bfs, dfs, astar
 
 MOVES = [ #Right,Left,Down,Up
     (0, 1),
@@ -14,8 +14,21 @@ def main():
     #Start [0,0] , End [n-1,m-1]
     start = (0,0)
     end = (len(G)-1, len(G[0])-1)
-    b = dfs(start, end)
-    print(b.search(PossiblePoints))
+    h = lambda point: sum(x-y for x,y in zip(end, point))
+    A = astar(start, end, h)
+
+    print('astart path:', A.search(PossiblePoints))
+    print('astar cost of end', A.cost[end])
+
+    B = bfs(start, end)
+
+    print('bfs path:', B.search(PossiblePoints))
+    print('bfs cost of end', B.cost[end])
+
+    D = dfs(start, end)
+
+    print('dfs path:', D.search(PossiblePoints))
+    print('dfs cost of end', D.cost[end])
 
 def isValid(point):
     return 0<=point[0]<len(G) and 0<=point[1]<len(G[0])  and (not G[point[0]][point[1]])
--- generics/generic.py
+++ generics/generic.py
@@ -1,28 +1,53 @@
 
 from collections import defaultdict, deque
+from heapq import heappop, heappush
+import operator
+
 
 class generic_search:
-    def __init__(self, start, end):
+    def __init__(
+        self, 
+        start, 
+        end, 
+        costF=lambda point, 
+            pre_point, pre_point_cost: pre_point_cost+1):
+        
         self.start = start
         self.end = end
-
+        self.costF = costF
+        self.get_point_from_item = lambda x: x
 
     def search(self, nextPointsFunction, parent={}):
         d = self._get_ds()
         visited = defaultdict(int)
+        cost = {}
+        
+        self.parent = parent
+        self.cost = cost
+        
+        cost[self.start] = 0
         
         # add start point 
         self._add_point(d, self.start)
-        
+
+
         while len(d) > 0:
-            current_point = self._get_next_item(d)
+            current_item = self._get_next_item(d)
+            current_point = self.get_point_from_item(current_item)
+            cur_cost = cost[current_point]
 
             # check solution is found
             if current_point == self.end:
                 return self.get_path(parent, current_point)
 
             # get next points
-            new_points = self._get_new_points(current_point, nextPointsFunction)
+            new_points = nextPointsFunction(current_point)
+
+            # update costs of points 
+            for point in new_points:
+                cost[point] = min(self.costF(point, current_point, cur_cost), 
+                        cost.get(point, float('inf')))
+
             # discard visited points
             not_visited_points = filter(lambda p: not visited[p], new_points)
 
@@ -70,3 +95,35 @@ class dfs(generic_search):
     
     def _get_new_points(self, current_point, nextPointsFunction):
         return nextPointsFunction(current_point)
+
+class astar(generic_search):
+    
+    def __init__( 
+        self, 
+        start, 
+        end,
+        h,
+        costF=lambda point, 
+            pre_point, pre_point_cost: pre_point_cost+1):
+
+        self.start = start
+        self.end = end
+        self.h = h
+        self.costF = costF
+        self.get_point_from_item = operator.itemgetter(1)
+
+    
+    def _get_ds(self):
+        return []
+
+    def _get_next_item(self, ds):
+        return heappop(ds)
+
+    def _add_point(self, ds, point):
+        # points will be added as tuple of heuristic and data
+        item_cost = self.cost[point]
+        item_h = self.h(point)
+        ds.append((item_cost+item_h, point))
+    
+    def _get_new_points(self, current_item, nextPointsFunction):
+        return nextPointsFunction(current_item[1])
